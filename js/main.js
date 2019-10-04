9ef7c167f6e509da0e4a41e4b04e22be6b4b41fe
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-03 20:41:49 -0700

    Add function to update current value of padding, blur and color.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// Pass event and use 'event.currentTarget' instead of 'this' because of the arrow function.
+const handleUpdate = event => {
+    // the empty string is for color value.
+    const dataSizing = event.currentTarget.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--base-${event.currentTarget.name}`, event.currentTarget.value + dataSizing);
+    switch(event.currentTarget.name) {
+        case 'padding' :
+            document.getElementById('show-padding').innerHTML = event.currentTarget.value;
+        case 'blur' :
+            document.getElementById('show-blur').innerHTML = event.currentTarget.value;
+    }
+};
+
+(function() {
+    const inputs = document.querySelectorAll('.container input');
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+}());
